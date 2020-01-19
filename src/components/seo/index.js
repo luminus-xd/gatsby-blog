d426d4d0269c9718922b0825dc56d32bf6cfa9f9
eleline <ggggepicstar@gmail.com>
2020-01-20 00:52:28 +0900

    user-scalable=no を削除

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -53,7 +53,7 @@ function SEO({
         },
         {
           name: `viewport`,
-          content: `width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0`,
+          content: `width=device-width,initial-scale=1`,
         },
         {
           name: `google-site-verification`,
